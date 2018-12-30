774708469494e93437faacb1ef208fa262b11b57
Alejandro E. Rendon <alejorendon@utp.edu.co>
2018-12-30 17:57:36 -0500

    Added basic information from github

--- getData.py
+++ getData.py
@@ -1,14 +1,60 @@
+import os
 import configparser
-from github import Github
+import requests
+import json
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 secret_key = config['DEFAULT']['GITHUB_KEY']
+username = config['DEFAULT']['USERNAME']
 
-# or using an access token
-g = Github(secret_key)
+user = {}
+repositories = []
+organizations = []
 
-# Then play with your Github objects:
-for repo in g.get_user().get_organization():
-    print(repo)
+# Check if Data folder exist
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+def writeFile(filename, data):
+    f = open(filename, 'w')
+    f.write(json.dumps(data))
+    f.close()
+
+def getUser():
+    r = requests.get("https://api.github.com/users/" + username)
+
+    if (r.ok):
+        return json.loads(r.text or r.content)
+    else:
+        return {}
+
+def getRepositories():
+    r = requests.get("https://api.github.com/users/" + username + "/repos")
+
+    if (r.ok):
+        return json.loads(r.text or r.content)
+    else:
+        return []
+
+def getOrganizations():
+    r = requests.get("https://api.github.com/users/" + username + "/orgs")
+
+    if (r.ok):
+        return json.loads(r.text or r.content)
+    else:
+        return []
+
+def main():
+    user = getUser()
+    writeFile('./data/user.json', user)
+    repositories = getRepositories()
+    writeFile('./data/repositories.json', repositories)
+    organizations = getOrganizations()
+    writeFile('./data/organizations.json', organizations)
+
+    print(user)
+
+if __name__ == '__main__':
+    main()
